13155473b46e955dc58fba1583955372deeae3af
777hanh777hanh <thanhthientran151100@gmail.com>
2024-03-06 18:54:05 +0700

    create my Carousel

--- main.js
+++ main.js
@@ -1,56 +1,234 @@
-// Lấy danh sách các item trong carousel
-const carouselItems = document.querySelectorAll('.carousel-item');
-
-// Xác định hành động khi nhấn nút next
-document.querySelector('.carousel-control-next').addEventListener('click', () => {
-    // Tìm item hiện tại có class 'active'
-    const currentItem = document.querySelector('.carousel-item.active');
-
-    // Tìm item kế tiếp của item hiện tại
-    const nextItem = currentItem.nextElementSibling;
-
-    // Thêm class 'carousel-item-start' vào current item và next item
-    nextItem.classList.add('carousel-item-next');
-    reflow(nextItem);
-    currentItem.classList.add('carousel-item-start');
-    nextItem.classList.add('carousel-item-start');
-
-    nextItem.addEventListener(
-        'transitionend',
-        () => {
-            nextItem.classList.remove('carousel-item-start', 'carousel-item-next');
-            nextItem.classList.add('active');
-
-            currentItem.classList.remove('active');
-        },
-        { once: true },
-    );
-});
-
+// Hàm `reflow` làm cho trình duyệt layout lại
 const reflow = (element) => {
-    console.log('reflow');
     element.offsetHeight;
 };
 
-document.querySelector('.carousel-control-prev').addEventListener('click', () => {
-    const currentItem = document.querySelector('.carousel-item.active');
-    // const prevItem = currentItem.previousElementSibling;
-    currentItem.classList.add('carousel-item-next');
-    reflow(currentItem);
-    currentItem.classList.add('carousel-item-start');
-    // prevItem.classList.add('carousel-item-prev');
-    // reflow(prevItem);
-    // // currentItem.classList.add('carousel-item-start');
-    // // prevItem.classList.add('carousel-item-start');
+// Danh sách các carousel đang chuyển động
+let listRedirecting = [];
 
-    // prevItem.addEventListener(
-    //     'transitionend',
-    //     () => {
-    //         prevItem.classList.remove('carousel-item-start', 'carousel-item-prev');
-    //         prevItem.classList.add('active');
+// Hàm loại bỏ carousel khỏi danh sách chuyển động
+const removeFromListRedirecting = (target) => {
+    listRedirecting = listRedirecting.filter((item) => item !== target);
+};
 
-    //         currentItem.classList.remove('active');
-    //     },
-    //     { once: true },
-    // );
+// Xử lý sự kiện khi nhấn nút "Next"
+const allControlNext = document.querySelectorAll('.carousel-control-next');
+allControlNext.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        // Kiểm tra xem carousel có trong danh sách chuyển động không
+        if (!listRedirecting.includes(item.dataset.target)) {
+            next(item.dataset.target);
+        }
+    });
 });
+
+// Xử lý sự kiện khi nhấn nút "Prev"
+const allControlPrev = document.querySelectorAll('.carousel-control-prev');
+allControlPrev.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        // Kiểm tra xem carousel có trong danh sách chuyển động không
+        if (!listRedirecting.includes(item.dataset.target)) {
+            prev(item.dataset.target);
+        }
+    });
+});
+
+// Hàm chuyển đến phần tử kế tiếp trong carousel
+const next = (target) => {
+    // Thêm carousel vào danh sách chuyển động
+    listRedirecting.push(target);
+
+    // Lấy carousel dựa vào biến target
+    const carouselContainer = document.querySelector(target);
+    // Kiểm tra xem carousel có tồn tại không
+    if (carouselContainer) {
+        // Lấy tất cả carousel-item trong carousel
+        const carouselItems = carouselContainer.querySelectorAll('.carousel-item');
+
+        // Kiểm tra xem có nhiều hơn 1 item trong carousel không
+        if (carouselItems.length > 1) {
+            // Tìm item hiện tại có class 'active'
+            const currentItem = carouselContainer.querySelector('.carousel-item.active');
+
+            // Tìm item kế tiếp của item hiện tại
+            let nextItem = currentItem.nextElementSibling;
+            if (!nextItem) {
+                // Nếu không có item kết tiếp thì lấy item đầu tiên
+                nextItem = carouselItems[0];
+            }
+
+            // Thêm class 'carousel-item-next' vào nextItem
+            nextItem.classList.add('carousel-item-next');
+
+            reflow(nextItem);
+
+            // Thêm class 'carousel-item-start' vào current item và next item
+            currentItem.classList.add('carousel-item-start');
+            nextItem.classList.add('carousel-item-start');
+
+            // Xử lý sự kiện khi chuyển động kết thúc
+            nextItem.addEventListener(
+                'transitionend',
+                () => {
+                    // xoá class 'carousel-item-start' của nextItem
+                    nextItem.classList.remove('carousel-item-start', 'carousel-item-next');
+                    // thêm class 'active' cho nextItem
+                    nextItem.classList.add('active');
+
+                    // xoá class 'active' và 'carousel-item-start' của currentItem
+                    currentItem.classList.remove('active', 'carousel-item-start');
+
+                    // xoá carousel trong list đang chuyển động
+                    removeFromListRedirecting(target);
+                },
+                { once: true },
+            );
+        } else if (carouselItems.length > 0) {
+            // khi chỉ có 1 item trong carousel
+            directOnlyItem(target, 'next');
+        }
+    }
+};
+
+// Hàm chuyển đến phần tử trước đó trong carousel
+const prev = (target) => {
+    // Thêm carousel vào danh sách chuyển động
+    listRedirecting.push(target);
+
+    // Lấy carousel dựa vào biến target
+    const carouselContainer = document.querySelector(target);
+    // Kiểm tra xem carousel có tồn tại không
+    if (carouselContainer) {
+        // Lấy tất cả carousel-item trong carousel
+        const carouselItems = carouselContainer.querySelectorAll('.carousel-item');
+
+        // Kiểm tra xem có nhiều hơn 1 item trong carousel không
+        if (carouselItems.length > 1) {
+            // Tìm item hiện tại có class 'active'
+            const currentItem = carouselContainer.querySelector('.carousel-item.active');
+
+            // Tìm item kế tiếp của item hiện tại
+            let prevItem = currentItem.previousElementSibling;
+            if (!prevItem) {
+                // Nếu không có item kết tiếp thì lấy item cuối cùng
+                prevItem = carouselItems[carouselItems.length - 1];
+            }
+
+            // Thêm class 'carousel-item-prev' vào prevItem
+            prevItem.classList.add('carousel-item-prev');
+
+            reflow(prevItem);
+
+            // Thêm class 'carousel-item-end' vào current item và prev item
+            currentItem.classList.add('carousel-item-end');
+            prevItem.classList.add('carousel-item-end');
+
+            // Xử lý sự kiện khi chuyển động kết thúc
+            prevItem.addEventListener(
+                'transitionend',
+                () => {
+                    // xoá class 'carousel-item-end' của prevItem
+                    prevItem.classList.remove('carousel-item-end', 'carousel-item-prev');
+                    // thêm class 'active' cho prevItem
+                    prevItem.classList.add('active');
+
+                    // xoá class 'active' và 'carousel-item-end' của currentItem
+                    currentItem.classList.remove('active', 'carousel-item-end');
+
+                    // xoá carousel trong list đang chuyển động
+                    removeFromListRedirecting(target);
+                },
+                { once: true },
+            );
+        } else if (carouselItems.length > 0) {
+            // khi chỉ có 1 item trong carousel
+            directOnlyItem(target, 'prev');
+        }
+    }
+};
+
+const directOnlyItem = (target, direction) => {
+    // Lấy Carousel dựa vào biến target
+    const carouselContainer = document.querySelector(target);
+    // Kiểm tra xem carousel có tồn tại không
+    if (carouselContainer) {
+        // Lấy item hiện tại có class 'active'
+        const currentItem = carouselContainer.querySelector('.carousel-item.active');
+        // Clone item hiện tại
+        const copyItem = currentItem.cloneNode(true);
+
+        // Xoá class 'active' của item clone
+        copyItem.classList.remove('active');
+
+        // Lấy carousel-inner chứa các thẻ carousel-item
+        const carouselInner = carouselContainer.querySelector('.carousel-inner');
+
+        // Đặt item clone vào carousel-inner
+        carouselInner.appendChild(copyItem);
+
+        // Thêm class 'carousel-item-next' hoặc 'carousel-item-prev' vào item clone
+        copyItem.classList.add(`carousel-item-${direction || ''}`);
+
+        reflow(copyItem);
+
+        // Nếu direction là 'next' thì thêm class 'carousel-item-start' vào item hiện tại và item clone
+        if (direction === 'next') {
+            currentItem.classList.add('carousel-item-start');
+            copyItem.classList.add('carousel-item-start');
+
+            // Xử lý sự kiện khi chuyển động kết thúc
+            copyItem.addEventListener(
+                'transitionend',
+                () => {
+                    // xoá class 'carousel-item-start' của item clone
+                    copyItem.classList.remove('carousel-item-start', `carousel-item-${direction}`);
+                    // thêm class 'active' cho item clone
+                    copyItem.classList.add('active');
+                    // xoá class 'active' và 'carousel-item-start' của item hiện tại
+                    currentItem.classList.remove('active', 'carousel-item-start');
+
+                    // xoá item hiện tại khỏi carousel-inner
+                    carouselInner.removeChild(currentItem);
+
+                    // xoá carousel trong list đang chuyển động
+                    removeFromListRedirecting(target);
+                },
+                { once: true },
+            );
+        } else if ((direction = 'prev')) {
+            // Nếu direction là 'prev' thì thêm class 'carousel-item-end' vào item hiện tại và item clone
+
+            // Thêm class 'carousel-item-end' vào item hiện tại và item clone
+            currentItem.classList.add('carousel-item-end');
+            copyItem.classList.add('carousel-item-end');
+
+            // Xử lý sự kiện khi chuyển động kết thúc
+            copyItem.addEventListener(
+                'transitionend',
+                () => {
+                    // xoá class 'carousel-item-end' của item clone
+                    copyItem.classList.remove('carousel-item-end', 'carousel-item-prev');
+                    // thêm class 'active' cho item clone
+                    copyItem.classList.add('active');
+                    // xoá class 'active' và 'carousel-item-end' của item hiện tại
+                    currentItem.classList.remove('active', 'carousel-item-end');
+
+                    // xoá item hiện tại khỏi carousel-inner
+                    carouselInner.removeChild(currentItem);
+
+                    // xoá carousel trong list đang chuyển động
+                    removeFromListRedirecting(target);
+                },
+
+                // Chỉ bắt sự kiện 1 lần
+                { once: true },
+            );
+        }
+    }
+};
--- readme.md
+++ readme.md
@@ -1,29 +1,35 @@
--   Trick để `Trình duyệt tính toán lại (reflow)`
+# My Carousel
 
-    ```js
-    const reflow = (element) => {
-        element.offsetHeight;
-    };
-    ```
+-   carousel: có class 'carousel' và id
+-   carousel-inner: chứa các 'carousel-item'
+-   'carousel-item' có chứa active sẽ được hiển thị lên
+-   nút có class 'carousel-control-prev' di chuyển sang trái
+-   nút có class 'carousel-control-next' di chuyển sang phải
+-   mỗi nút di chuyển phải có property `data-target` có giá trị là **`id` của carousel**
 
-    -   trick này là do khi truy cập một số thuộc tính của một phần tử có thể kích hoạt trình duyệt thực hiện lại bố cục hoặc vẽ lại, những thao tác này cần thiết để cập nhật bố cục trực quan của trang.
-    -   Thuộc tính `offsetHeight` được sử dụng để lấy chiều cao của một phần tử, bao gồm cả padding, border và thanh cuộn, nhưng không bao gồm margin. Khi truy cập vào `offsetHeight`, trình duyệt cần phải tính toán lại bố cục của trang, có thể bao gồm việc tính toán lại vị trí và kích thước của các phần tử khác trên trang.
-    -   `reflow` ở đây giúp thay đổi CSS được áp dụng ngay tức thì mà không cần chờ cho đến khi trình duyệt thực hiện một reflow tự động sau một loạt các thay đổi
+```html
+<div id="carouselExampleId" class="carousel slide">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <h2>Item 1</h2>
+        </div>
+        <div class="carousel-item">
+            <h2>Item 2</h2>
+        </div>
+        <div class="carousel-item">
+            <h2>Item 3</h2>
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-target="#carouselExampleId">
+        <span>Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-target="#carouselExampleId">
+        <span>Next</span>
+    </button>
+</div>
+```
 
-    -   ví dụ:
+<br />
+<hr />
 
-        ```js
-        // nextItem -> display: none;
-
-        nextItem.classList.add('carousel-item-next');
-        // carousel-item-next -> display: block; translateX -> 100%
-
-        // áp dụng css ngay lập tức
-        reflow(nextItem);
-
-        nextItem.classList.add('carousel-item-start');
-        // carousel-item-start -> translateX -> -100%
-        ```
-
-        Tại ví dụ trên, nếu không có reflow khi nextItem chạy sang phải thì ta chỉ thấy item đứng yên và hiện lên.
-        Do áp dụng reflow sau khi item chạy sang phải. Nên khi này item ở bên phải và áp dụng di chuyển sang trái. Ta sẽ thấy item chạy từ phải sang trái và áp dụng transition.
+## [View demo](https://777hanh777hanh.github.io/my_carousel-slider/)
